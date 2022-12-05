747f4b0eb0794cb0c991d47cfa2289e872ed804e
waqas-on-github <waqasvu892@gmail.com>
2022-12-06 03:30:57 +0500

    Apple.com clone day01

---
+++ week4_tailwind_css/tailwind_projects/Apple-webpage-clone/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+      fontFamily: {
+        sans: [
+          "Inter var, sans-serif",
+          { fontFeatureSettings: '"cv11", "ss01"' },
+        ],
+        Nerko:[
+            "Nerko one",
+            { fontFeatureSettings: '"cv11", "ss01"' },
+        ]
+        ,
+        popins:[
+            "Poppins,sans-serif"
+        ]
+        
+
+      },
+    },
+  }
