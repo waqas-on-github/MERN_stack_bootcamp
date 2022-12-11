c97f45920b7e07444aac57cbb65fc4282d66a552
waqas-on-github <waqasvu892@gmail.com>
2022-12-11 22:52:28 +0500

    javascript-started

---
+++ week6_javascript-started/jsweek1/date.js
@@ -0,0 +1,12 @@
+console.log(" date code start here ")
+ const now = new Date()
+  console.log(now)
+  console.log(now.toString())
+  console.log(now.getFullYear())
+  console.log(now.getMonth())
+  console.log(now.getDay())
+  console.log(now.getHours())
+  console.log(now.getMinutes())
+  console.log(now.getSeconds())
+
+  console.log(" date code end here ")
---
+++ week6_javascript-started/jsweek1/math.js
@@ -0,0 +1,12 @@
+console.log('math code start here ')
+let pi=Math.PI
+let num = 9.5
+console.log(pi)
+console.log(Math.floor(pi))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+console.log(Math.min(num))
+console.log(Math.max(num))
+// imp
+console.log(Math.round(Math.random()* 10))
+console.log('math code end here ')
