52bede9eae63515b0b3dcd5ff5d6a26acdceffe3
waqas-on-github <waqasvu892@gmail.com>
2023-01-17 23:50:48 +0500

    scope topic for today

---
+++ week9-js-deepfoundations/scope/scope-quiz.js
@@ -0,0 +1,13 @@
+var teacher ="kyle"
+
+
+function  otherclass () {
+     teacher="suzy"    // -------- teacher varaiable in function scope  but it wiil be reassigned cuz we have not declared 
+     topic ='react'    // ------> we did not  declare it so itll be auto declared when look up process hapen globally 
+    console.log(teacher);
+}
+
+
+otherclass()
+console.log(teacher);  // suzy
+console.log(topic);   // react
---
+++ week9-js-deepfoundations/scope/scope01.js
@@ -0,0 +1,19 @@
+
+
+var teacher ="kyle"
+
+
+function  otherclass () {
+    var teacher="suzy"
+    console.log(teacher);
+}
+
+
+function ask () {
+    var question  ="why?"
+    console.log(question);
+}
+
+
+otherclass()
+ask()
