974f114d0a457f39820580c2180a9dcc75c70093
waqas-on-github <waqasvu892@gmail.com>
2023-02-16 23:31:43 +0500

    weather app getting started

--- week11 -js-projects/weather-app/app.js
+++ week11 -js-projects/weather-app/app.js
@@ -1,7 +1,13 @@
 
 const key="620549b806f64268a5c221842231102"
-let data =fetch(`http://api.weatherapi.com/v1 `)
-.then((data) => data.json())
-.then((d) => {
-    console.log(d);
+let data =fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=London`)
+
+data.then((data) => data.json()).then((data) => {
+    console.log(data);
 })
+
+
+// get input for serching 
+
+let form =document.querySelector('form')
+console.log(form);
