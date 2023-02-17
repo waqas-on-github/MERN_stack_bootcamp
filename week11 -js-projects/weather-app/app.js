0251529366c3184b3cfc2793f37f81853118cbd0
waqas-on-github <waqasvu892@gmail.com>
2023-02-17 23:50:57 +0500

    weather app small progress

--- week11 -js-projects/weather-app/app.js
+++ week11 -js-projects/weather-app/app.js
@@ -1,13 +1,66 @@
 
-const key="620549b806f64268a5c221842231102"
-let data =fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=London`)
 
-data.then((data) => data.json()).then((data) => {
-    console.log(data);
+const key="620549b806f64268a5c221842231102"
+//  selecting elements 
+let form =document.querySelector("form")
+let cityName =form.children[0]
+let weather=document.querySelector(".weather")
+
+
+
+// when user will click on submit button event will be triggerd and send request to weather api 
+
+form[1].addEventListener('click',(e) => {
+    e.preventDefault() 
+    if(cityName.value.length!==0){
+        getweatherdata(cityName.value)
+    }
+    else{
+        
+        console.log("enter city name please")
+   }
 })
 
+// getting weather data function 
+ function getweatherdata(city){
+     fetch(`http://api.weatherapi.com/v1/future.json?key=${key}&q=${city}&dt=2023-03-04`)
+     .then((res) => res.json())
+     .then((data) => {
+        displaytoui(data)
+     })
+    
+ }
 
-// get input for serching 
 
-let form =document.querySelector('form')
-console.log(form);
+
+
+
+function displaytoui (data) {
+
+ displayheader(data)
+
+
+  
+}
+
+function displayheader(data,){
+let header=document.querySelector(".headcomponent")
+console.log(header.children);
+header.children[0].textContent=data.location.region
+header.children[1].firstElementChild.textContent=data.forecast.forecastday[0].day.mintemp_c 
+header.children[2].firstElementChild.firstElementChild.textContent=`H:${data.forecast.forecastday[0].day.maxtemp_c}`
+header.children[2].children[1].firstElementChild.textContent=`H:${data.forecast.forecastday[0].day.mintemp_c}`
+console.log();
+
+
+
+console.log();
+
+
+
+
+
+}
+
+
+
