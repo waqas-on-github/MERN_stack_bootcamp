a0d7e2a23d91541c7c1b1c6c344a7896b06f4325
waqas-on-github <waqasvu892@gmail.com>
2023-02-25 09:16:27 +0500

    swiggy api project

---
+++ week13-to-18-react.js/react/swiggy-api/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Cards from './components/Cards';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+     <React.Fragment>
+
+       <Cards></Cards>
+     </ React.Fragment>
+    );
+  }
+}
+
+export default App;
