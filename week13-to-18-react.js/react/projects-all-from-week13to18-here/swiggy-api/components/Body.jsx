94272ab5b949f2eb356e4685bfbf5545514cdb76
waqas-on-github <waqasvu892@gmail.com>
2023-02-26 07:47:15 +0500

    adding filter funcationalty in swwigy api

---
+++ week13-to-18-react.js/react/projects-all-from-week13to18-here/swiggy-api/components/Body.jsx
@@ -0,0 +1,54 @@
+
+import React, { useEffect, useState } from 'react'
+import  {Img ,Url} from'../const'
+
+
+
+const Body=() => {
+
+ let [res,setres] =useState()
+ let [inpval,setinpval] =useState('');
+
+useEffect(() => {
+  getdata()
+},[inpval] )
+
+
+function filterData(res, inpval) {
+     let result = res?.filter((r) => (r?.data?.name?.includes(inpval)?r :null))
+     setres(result)
+   }
+  
+
+  async function getdata() {
+    let data =await  fetch(Url)
+    let json=await data.json()
+    setres(json?.data?.cards[2]?.data?.data?.cards)
+  
+ }
+
+return(
+  <div className='main' > 
+        <div className="serachbar">
+          <input type="text" placeholder='order food!' 
+          value={inpval} onChange={(e)=>{setinpval(e.target.value)}}  />
+          <button  onClick={()=>{filterData(res,inpval)}} > Order</button>
+         </div>
+
+
+          <div className="Cards">
+
+
+
+             {res?.map((r) => {   
+             return(
+               <div className='card-item' key={r?.data?.uuid}> 
+                     <img src={Img+r.data.cloudinaryImageId} />
+                     <h3> {r.data.name}</h3>
+               </div>
+             )})} 
+          </div>
+  </div>
+    )
+}
+export default Body
---
+++ week13-to-18-react.js/react/projects-all-from-week13to18-here/swiggy-api/const.js
@@ -0,0 +1,3 @@
+
+export  const Img="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+export const Url="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
