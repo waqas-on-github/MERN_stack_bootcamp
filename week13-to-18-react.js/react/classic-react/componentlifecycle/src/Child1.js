ab57cd6563246e3a7cf928b10221d8d9ed2b7cf0
waqas-on-github <waqasvu892@gmail.com>
2023-03-07 23:24:50 +0500

    component life cycle solidified

---
+++ week13-to-18-react.js/react/classic-react/componentlifecycle/src/Child1.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Child1 extends Component {
+
+constructor(props){
+    super(props)
+    console.log("child1 constructor called ")
+   this.state={
+     
+   } 
+}
+
+
+ async componentDidMount() {
+    console.log('child1 component didmount called ');
+
+   let data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+       data = await data.json()
+        this.setState(data)
+    console.log(data)
+ }
+
+
+ componentDidUpdate() {
+    console.log("child1 component didupdate called ")
+ }
+
+  render() {
+      {console.log('child1 render called')}
+    return (
+      <div>Child1</div>
+    )
+  }
+}
